cdc4f9d66b98b47731aeb43025461a784e48e4c7
elenfe <elenfe7@gmail.com>
2018-11-02 03:27:01 -0300

    part2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div class="container">
+      <img src="./assets/logo.png">
+      <img src="./assets/logo.png">
+    <router-view></router-view>
+    <button type="button" name="button" class="btn btn-primary">Olá Mundo</button>
+    </div>    
   </div>
 </template>
 
@@ -11,13 +15,4 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./assets/sass/app.scss" lang="sass"></style>
